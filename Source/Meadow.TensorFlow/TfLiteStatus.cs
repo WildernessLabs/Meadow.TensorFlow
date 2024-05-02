bcf76947ab99d48772b624d998b8bc00fb278c3c
halyssonJr <halysson1007@gmail.com>
2024-05-02 14:42:19 -0300

    add API description

--- Source/Meadow.TensorFlow/TfLiteStatus.cs
+++ Source/Meadow.TensorFlow/TfLiteStatus.cs
@@ -4,37 +4,51 @@ using System.Text;
 
 namespace TensorFlow.litemicro
 {
+    /// <summary>
+    /// Enum representing the internal states in TensorFlow Lite.
+    /// </summary>
     public enum TfLiteStatus
     {
+        /// <summary>
+        /// Generally refers to a successful execution in the runtime.
+        /// </summary>
         kTfLiteOk = 0,
 
-        // Generally referring to an error in the runtime (i.e. interpreter)
+        /// <summary>
+        /// Generally referring to an error in the runtime.
+        /// </summary>
         kTfLiteError = 1,
 
-        // Generally referring to an error from a TfLiteDelegate itself.
+        /// <summary>
+        /// Generally referring to an error from a TfLiteDelegate itself.
+        /// </summary>
         kTfLiteDelegateError = 2,
 
-        // Generally referring to an error in applying a delegate due to
-        // incompatibility between runtime and delegate, e.g., this error is returned
-        // when trying to apply a TF Lite delegate onto a model graph that's already
-        // immutable.
+        /// <summary>
+        /// Generally referring to an error in applying a delegate due to incompatibility 
+        /// between runtime and delegate.
+        /// </summary>
         kTfLiteApplicationError = 3,
 
-        // Generally referring to serialized delegate data not being found.
-        // See tflite::delegates::Serialization.
+        /// <summary>
+        /// Generally referring to serialized delegate data not being found.
+        /// </summary>
         kTfLiteDelegateDataNotFound = 4,
 
-        // Generally referring to data-writing issues in delegate serialization.
-        // See tflite::delegates::Serialization.
+        /// <summary>
+        /// Generally referring to data-writing issues in delegate serialization.
+        /// </summary>
         kTfLiteDelegateDataWriteError = 5,
 
-        // Generally referring to data-reading issues in delegate serialization.
-        // See tflite::delegates::Serialization.
+        /// <summary>
+        /// Generally referring to data-reading issues in delegate serialization.
+        /// </summary>
         kTfLiteDelegateDataReadError = 6,
 
-        // Generally referring to issues when the TF Lite model has ops that cannot be
-        // resolved at runtime. This could happen when the specific op is not
-        // registered or built with the TF Lite framework.
+        /// <summary>
+        /// Generally referring to issues when the TF Lite model has ops that cannot
+        /// be resolved at runtime.
+        /// </summary>
         kTfLiteUnresolvedOps = 7,
     }
 }
--- Source/Meadow.TensorFlow/TfLiteTensor.cs
+++ Source/Meadow.TensorFlow/TfLiteTensor.cs
@@ -2,13 +2,33 @@ using System;
 
 namespace TensorFlow.litemicro
 {
+    /// <summary>
+    /// Structure representing a tensor in TensorFlow Lite.
+    /// </summary>
     public struct TfLiteTensor
     {
         private IntPtr _handle;
+
+        /// <summary>
+        /// Constructor of the TfLiteTensor class.
+        /// </summary>
+        /// <param name="handle">The tensor handle.</param>
         public TfLiteTensor(IntPtr handle)
-            =>_handle = handle;
+            => _handle = handle;
+
+        /// <summary>
+        /// Implicit conversion from IntPtr to TfLiteTensor.
+        /// </summary>
+        /// <param name="handle">The tensor handle.</param>
+        /// <returns>The tensor corresponding to the handle.</returns>
         public static implicit operator TfLiteTensor(IntPtr handle)
             => new TfLiteTensor(handle);
+
+        /// <summary>
+        /// Implicit conversion from TfLiteTensor to IntPtr.
+        /// </summary>
+        /// <param name="tensor">The tensor to be converted.</param>
+        /// <returns>The handle of the tensor.</returns>
         public static implicit operator IntPtr(TfLiteTensor tensor)
             => tensor._handle;
     }
