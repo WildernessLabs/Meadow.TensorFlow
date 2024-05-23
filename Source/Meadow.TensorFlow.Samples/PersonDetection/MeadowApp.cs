69cf7cd96af0291b6cbf0accaeadb7fb97fea57c
halyssonJr <halysson1007@gmail.com>
2024-05-23 12:06:18 -0300

    add classify score

--- Source/Meadow.TensorFlow.Samples/PersonDetection/MeadowApp.cs
+++ Source/Meadow.TensorFlow.Samples/PersonDetection/MeadowApp.cs
@@ -2,6 +2,7 @@
 using Meadow;
 using Meadow.Devices;
 using System;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using TensorFlow.litemicro;
@@ -15,7 +16,7 @@ public class MeadowApp : App<F7FeatherV2>
     const int ArenaSize = 134 * 1024;
     TfLiteStatus tfLiteStatus;
     IntPtr interpreter;
-    TfLiteTensor input;
+    TfLiteTensor input, output;
     public readonly Image []images = 
     { 
         Image.LoadFromResource("Resources.no_person.bmp"),
@@ -47,15 +48,21 @@ public class MeadowApp : App<F7FeatherV2>
 
         var model_options = c_api_lite_micro.TfLiteMicroGetModel(ArenaSize, arena, model);
         if (model_options == null)
+        {
             Resolver.Log.Info("Failed to loaded the model");
+        }
 
         var interpreter_options = c_api_lite_micro.TfLiteMicroInterpreterOptionCreate(model_options);
         if (interpreter_options == null)
+        {
             Resolver.Log.Info("Failed to create interpreter option");
+        }
 
         interpreter = c_api_lite_micro.TfLiteMicroInterpreterCreate(interpreter_options, model_options);
         if (interpreter == null)
+        {
             Resolver.Log.Info("Failed to Interpreter");
+        }
 
         tfLiteStatus = c_api_lite_micro.TfLiteMicroInterpreterAllocateTensors(interpreter);
         if (tfLiteStatus != TfLiteStatus.kTfLiteOk)
@@ -64,13 +71,46 @@ public class MeadowApp : App<F7FeatherV2>
         }
 
         input = c_api_lite_micro.TfLiteMicroInterpreterGetInput(interpreter, 0);
+        CopyImageToTensor(images[1], input);
 
         return Task.CompletedTask;
     }
 
     public override Task Run()
     {
+        TfLiteStatus tfLiteStatus;
+        tfLiteStatus = c_api_lite_micro.TfLiteMicroInterpreterInvoke(interpreter);
+        if (tfLiteStatus != TfLiteStatus.kTfLiteOk)
+        {
+            Resolver.Log.Info("Invoke failed");
+            return Task.CompletedTask;
+        }
+
+        output = c_api_lite_micro.TfLiteMicroInterpreterGetOutput(interpreter, 0);
+        int outputDimsSize = c_api_lite_micro.TfLiteMicroDimsSizeData(output);
+        int outputData0 = c_api_lite_micro.TfLiteMicroDimsData(output, 0);
+        int outputDimsData0Size = c_api_lite_micro.TfLiteMicroDimsData(output, 1);
+        TfLiteType tfLiteType = c_api_lite_micro.TfLiteMicroGetType(output);
+
+        Resolver.Log.Info($" Dims Size:{outputDimsSize}, Data 0: {outputData0},  Size:{outputDimsData0Size}");
+        int personScore = (int)c_api_lite_micro.TfLiteMicroGeInt8tData(output, 1);
+        int noPersonScore = (int)c_api_lite_micro.TfLiteMicroGeInt8tData(output, 0);
+        Resolver.Log.Info($"Score \n - Person:{personScore}\n - No Person:{noPersonScore}");
         Resolver.Log.Info("Sample completed");
         return Task.CompletedTask;
+    }
+
+    public static void CopyImageToTensor(Image img, TfLiteTensor tensor)
+    {
+        int width = img.DisplayBuffer.Width;
+        int height = img.DisplayBuffer.Height;
+        using var memStream = new MemoryStream(img.DisplayBuffer.Buffer);
+        Resolver.Log.Info($"Image Infos - width: {width}, height: {height} , size: {memStream.Length}");
+        int index = 0;
+        memStream.Seek(0, SeekOrigin.Begin);
+        while(index < memStream.Length)
+        {
+            c_api_lite_micro.TfLiteMicroSetInt8Data(tensor, index++, (sbyte)memStream.ReadByte());
+        }
     }
 }
