a4f5b609ed99d4030609dbfaea584f079dabd7c0
halyssonJr <halysson1007@gmail.com>
2024-05-28 18:05:19 -0300

    convert the stream to an image

--- Source/Meadow.TensorFlow.Samples/PersonDetection/MeadowApp.cs
+++ Source/Meadow.TensorFlow.Samples/PersonDetection/MeadowApp.cs
@@ -9,12 +9,16 @@ using System.Threading.Tasks;
 using TensorFlow.litemicro;
 
 using Meadow.Foundation.Graphics;
+using Meadow.Foundation.Graphics.Buffers;
 using Meadow.Foundation.Graphics.MicroLayout;
+using Meadow.Foundation.Displays;
 using Meadow.Peripherals.Displays;
 using Meadow.Foundation.Sensors.Camera;
 
 using BitMiracle.LibJpeg;
 using SimpleJpegDecoder;
+using System.Reflection;
+// using System.Drawing.Bitmap;
 
 namespace PersonDetection;
 
@@ -46,95 +50,96 @@ public class MeadowApp : App<F7CoreComputeV2>
             Resolver.Log.Info("Resolution successfully changed");
         }
 
-        Resolver.Log.Info("Initialize TensorFlow ...");
+        // Resolver.Log.Info("Initialize TensorFlow ...");
 
-        IntPtr model = Marshal.AllocHGlobal(personDetectionModel.GetSize() * sizeof(int));
+        // IntPtr model = Marshal.AllocHGlobal(personDetectionModel.GetSize() * sizeof(int));
 
-        if (model == IntPtr.Zero)
-        {
-            Resolver.Log.Info("Failed to allocated model");
-            return base.Initialize();
-        }
+        // if (model == IntPtr.Zero)
+        // {
+        //     Resolver.Log.Info("Failed to allocated model");
+        //     return base.Initialize();
+        // }
 
-        IntPtr arena = Marshal.AllocHGlobal(ArenaSize * sizeof(int));
+        // IntPtr arena = Marshal.AllocHGlobal(ArenaSize * sizeof(int));
 
-        if (arena == IntPtr.Zero)
-        {
-            Resolver.Log.Info("Failed to allocated arena");
-            Marshal.FreeHGlobal(model);
-            return base.Initialize();
-        }
+        // if (arena == IntPtr.Zero)
+        // {
+        //     Resolver.Log.Info("Failed to allocated arena");
+        //     Marshal.FreeHGlobal(model);
+        //     return base.Initialize();
+        // }
 
-        Marshal.Copy(personDetectionModel.GetData(), 0, model, personDetectionModel.GetSize());
+        // Marshal.Copy(personDetectionModel.GetData(), 0, model, personDetectionModel.GetSize());
 
-        var model_options = c_api_lite_micro.TfLiteMicroGetModel(ArenaSize, arena, model);
-        if (model_options == null)
-        {
-            Resolver.Log.Info("Failed to loaded the model");
-        }
+        // var model_options = c_api_lite_micro.TfLiteMicroGetModel(ArenaSize, arena, model);
+        // if (model_options == null)
+        // {
+        //     Resolver.Log.Info("Failed to loaded the model");
+        // }
 
-        var interpreter_options = c_api_lite_micro.TfLiteMicroInterpreterOptionCreate(model_options);
-        if (interpreter_options == null)
-        {
-            Resolver.Log.Info("Failed to create interpreter option");
-        }
+        // var interpreter_options = c_api_lite_micro.TfLiteMicroInterpreterOptionCreate(model_options);
+        // if (interpreter_options == null)
+        // {
+        //     Resolver.Log.Info("Failed to create interpreter option");
+        // }
 
-        interpreter = c_api_lite_micro.TfLiteMicroInterpreterCreate(interpreter_options, model_options);
-        if (interpreter == null)
-        {
-            Resolver.Log.Info("Failed to Interpreter");
-        }
+        // interpreter = c_api_lite_micro.TfLiteMicroInterpreterCreate(interpreter_options, model_options);
+        // if (interpreter == null)
+        // {
+        //     Resolver.Log.Info("Failed to Interpreter");
+        // }
 
-        tfLiteStatus = c_api_lite_micro.TfLiteMicroInterpreterAllocateTensors(interpreter);
-        if (tfLiteStatus != TfLiteStatus.kTfLiteOk)
-        {
-            Resolver.Log.Info("Failed to allocate tensors");
-        }
+        // tfLiteStatus = c_api_lite_micro.TfLiteMicroInterpreterAllocateTensors(interpreter);
+        // if (tfLiteStatus != TfLiteStatus.kTfLiteOk)
+        // {
+        //     Resolver.Log.Info("Failed to allocate tensors");
+        // }
 
-        input = c_api_lite_micro.TfLiteMicroInterpreterGetInput(interpreter, 0);
-        CopyImageToTensor(images[1], input);
+        // input = c_api_lite_micro.TfLiteMicroInterpreterGetInput(interpreter, 0);
+        // CopyImageToTensor(images[1], input);
 
         return Task.CompletedTask;
     }
 
-    public override Task Run()
+    public override async Task Run()
     {
-        _ = TakePicture();
+        await TakePicture();
 
-        TfLiteStatus tfLiteStatus;
-        tfLiteStatus = c_api_lite_micro.TfLiteMicroInterpreterInvoke(interpreter);
-        if (tfLiteStatus != TfLiteStatus.kTfLiteOk)
-        {
-            Resolver.Log.Info("Invoke failed");
-            return Task.CompletedTask;
-        }
+        // TfLiteStatus tfLiteStatus;
+        // tfLiteStatus = c_api_lite_micro.TfLiteMicroInterpreterInvoke(interpreter);
+        // if (tfLiteStatus != TfLiteStatus.kTfLiteOk)
+        // {
+        //     Resolver.Log.Info("Invoke failed");
+        //     return Task.CompletedTask;
+        // }
 
-        output = c_api_lite_micro.TfLiteMicroInterpreterGetOutput(interpreter, 0);
-        int outputDimsSize = c_api_lite_micro.TfLiteMicroDimsSizeData(output);
-        int outputData0 = c_api_lite_micro.TfLiteMicroDimsData(output, 0);
-        int outputDimsData0Size = c_api_lite_micro.TfLiteMicroDimsData(output, 1);
-        TfLiteType tfLiteType = c_api_lite_micro.TfLiteMicroGetType(output);
+        // output = c_api_lite_micro.TfLiteMicroInterpreterGetOutput(interpreter, 0);
+        // int outputDimsSize = c_api_lite_micro.TfLiteMicroDimsSizeData(output);
+        // int outputData0 = c_api_lite_micro.TfLiteMicroDimsData(output, 0);
+        // int outputDimsData0Size = c_api_lite_micro.TfLiteMicroDimsData(output, 1);
+        // TfLiteType tfLiteType = c_api_lite_micro.TfLiteMicroGetType(output);
 
-        Resolver.Log.Info($" Dims Size:{outputDimsSize}, Data 0: {outputData0},  Size:{outputDimsData0Size}");
-        int personScore = (int)c_api_lite_micro.TfLiteMicroGeInt8tData(output, 1);
-        int noPersonScore = (int)c_api_lite_micro.TfLiteMicroGeInt8tData(output, 0);
-        Resolver.Log.Info($"Score \n - Person:{personScore}\n - No Person:{noPersonScore}");
-        Resolver.Log.Info("Sample completed");
+        // Resolver.Log.Info($" Dims Size:{outputDimsSize}, Data 0: {outputData0},  Size:{outputDimsData0Size}");
+        // int personScore = (int)c_api_lite_micro.TfLiteMicroGeInt8tData(output, 1);
+        // int noPersonScore = (int)c_api_lite_micro.TfLiteMicroGeInt8tData(output, 0);
+        // Resolver.Log.Info($"Score \n - Person:{personScore}\n - No Person:{noPersonScore}");
+        // Resolver.Log.Info("Sample completed");
 
-        return Task.CompletedTask;
+        // return Task.CompletedTask;
     }
 
     async Task TakePicture()
     {
-        Resolver.Log.Info($"Image size is {camera.GetCaptureResolution()}");
-
+        Resolver.Log.Info("Take a picture");
         camera.CapturePhoto();
 
         using var jpegStream = await camera.GetPhotoStream();
+        // var jpeg = new JpegImage(jpegStream);
 
-        var decoder = new JpegDecoder();
-        var jpg = decoder.DecodeJpeg(jpegStream);
-        Resolver.Log.Info($"Jpeg decoded is {jpg.Length} bytes, W: {decoder.Width}, H: {decoder.Height}");
+        // var decoder = new JpegDecoder();
+        // var jpeg_decoder = decoder.DecodeJpeg(photoData);
+        // IPixelBuffer pBuffer = new BufferRgb888(decoder.Width, decoder.Height, jpeg_decoder);
+        var image = Image.LoadFromStream(jStream);
     }
 
     public static void CopyImageToTensor(Image img, TfLiteTensor tensor)
